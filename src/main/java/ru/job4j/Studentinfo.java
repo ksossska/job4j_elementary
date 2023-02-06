70d7ec894358030a50961e2a16a8e56ad5d3e8b6
1 <kilo.gta@mail.ru>
2023-02-06 14:15:23 +0500

    Фиксация изменений на github [#243564 #66430]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[]args){
+        System.out.println("Ksosa");
+        System.out.println("16.08.2001");
+    }
+}
