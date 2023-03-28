125735ffd7935944eb51e223d79b42a4c1615ed1
1 <kilo.gta@mail.ru>
2023-03-28 14:46:41 +0500

    Фиксация изменений на github [#243564 #66430]

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calc(int n) {
+        int sum = 1;
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+        }
+        return sum;
+    }
+}
